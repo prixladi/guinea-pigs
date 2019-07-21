71e98055a6b26477dcf47e59242c150f2445ea5e
Ladislav Prix <prixladi@fit.cvut.cz>
2019-07-21 17:37:22 +0200

    Updates

---
+++ precache-manifest.e9931fb36e7e0cff5287bfac9798d2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850f1baaae01cca8f38be4d0ffd01128",
+    "url": "/guinea_pigs/index.html"
+  },
+  {
+    "revision": "b52a999a50049b27636e",
+    "url": "/guinea_pigs/static/css/2.f74a8007.chunk.css"
+  },
+  {
+    "revision": "98d0b31b3905f9f463cb",
+    "url": "/guinea_pigs/static/css/main.d6aa9b15.chunk.css"
+  },
+  {
+    "revision": "b52a999a50049b27636e",
+    "url": "/guinea_pigs/static/js/2.aee3da3c.chunk.js"
+  },
+  {
+    "revision": "98d0b31b3905f9f463cb",
+    "url": "/guinea_pigs/static/js/main.b3177bdb.chunk.js"
+  },
+  {
+    "revision": "e93cbc88720a19e2ad7d",
+    "url": "/guinea_pigs/static/js/runtime~main.11c40913.js"
+  }
+]);
